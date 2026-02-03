763e90e1e0d71f7dbd7a6ffe53777ddc72d80356
Ali Arsal <ali.arsal1512@gmail.com>
2026-02-02 19:02:21 -0500

    second commit

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,10 +1,7 @@
-# run.py
-import os
+# backend/wsgi.py
 from app import create_app
 
 app = create_app()
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    debug = os.environ.get('FLASK_ENV') == 'development'
-    app.run(host='0.0.0.0', port=port, debug=debug)
+if __name__ == "__main__":
+    app.run()
